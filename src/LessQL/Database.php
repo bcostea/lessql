813c809a3bdc4667a3801ccfa80bd5373f86392e
Bogdan Costea <bogdan.costea@xogito.com>
2020-12-25 19:33:22 +0200

    Improve PostgreSQL compatibilty

--- src/LessQL/Database.php
+++ src/LessQL/Database.php
@@ -17,6 +17,11 @@ class Database
         // required for safety
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $this->pdo = $pdo;
+
+        // Improve PostgreSQL compatibilty
+        if ($this->pdo->getAttribute(\PDO::ATTR_DRIVER_NAME) === 'pgsql') {
+            $this->setIdentifierDelimiter('"');
+        }
     }
 
     /**
